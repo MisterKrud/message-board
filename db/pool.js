aa221b673a26ab35feec6c8ab935525fe5511380
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-12-29 09:42:27 +1100

    Add user and timeout limits in pool

--- db/pool.js
+++ db/pool.js
@@ -7,6 +7,9 @@ module.exports = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
+    max: 5,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
 })
 
 /*
