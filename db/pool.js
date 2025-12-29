ba62e94aaf85be1df5fd3c681d1a610d4154bd37
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-12-29 18:35:46 +1100

    Changes for deployment after deleting and starting again on server

--- db/pool.js
+++ db/pool.js
@@ -1,16 +1,26 @@
-require("dotenv").config();
-
 const { Pool } = require("pg");
+const fs = require("fs");
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 const connectionString =
-    process.env.NODE_ENV === "production"
+  process.env.NODE_ENV === "production"
     ? process.env.DATABASE_URL
     : process.env.DEV_DB_URL;
 
-const ssl = process.env.NODE_ENV === "production"
-  ? { rejectUnauthorized: false }
-  : false;
+const ssl =
+  process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: false }
+    : false;
 
-const pool = new Pool({ connectionString, ssl });
+const pool = new Pool({
+  connectionString,
+  ssl,
+  max: 5, // limit simultaneous connections
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+});
 
 module.exports = pool;
--- scripts/dbSetup.js
+++ scripts/dbSetup.js
@@ -1,20 +1,9 @@
+const { Client } = require("pg");
+const fs = require("fs");
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
-
-console.log("NODE_ENV:", process.env.NODE_ENV);
-console.log("DATABASE_URL:", process.env.DATABASE_URL);
-console.log("DEV_DB_URL:", process.env.DEV_DB_URL);
-console.log("ENV VARIABLES KEYS:", Object.keys(process.env));
-
-
-
-const { Client } = require("pg");
-
-
-
-
 
 const connectionString =
   process.env.NODE_ENV === "production"
@@ -26,29 +15,25 @@ const ssl =
     ? { rejectUnauthorized: false }
     : false;
 
-
-
-
-
 const client = new Client({ connectionString, ssl });
 
 async function main() {
   try {
     await client.connect();
-    console.log(`Connected to database`);
-
+    console.log(`Connected to ${process.env.NODE_ENV || "development"} database`);
 
     await client.query(`
       CREATE TABLE IF NOT EXISTS messages(
-id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  username VARCHAR (20),
-  message TEXT NOT NULL, added TIMESTAMP
-);
+        id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        username VARCHAR(20),
+        message TEXT NOT NULL,
+        added TIMESTAMP
+      );
 
-INSERT INTO messages (username, message, added)
- VALUES
- ('Montgomery', 'Hi there!', NOW()),
-('Agatha', 'Hello everyone', NOW());
+      INSERT INTO messages (username, message, added)
+      VALUES
+        ('Montgomery', 'Hi there!', NOW()),
+        ('Agatha', 'Hello everyone', NOW());
     `);
 
     console.log("Database setup complete!");
@@ -58,9 +43,5 @@ INSERT INTO messages (username, message, added)
     await client.end();
   }
 }
-
-console.log("NODE_ENV =", process.env.NODE_ENV);
-console.log("DATABASE_URL exists =", !!process.env.DATABASE_URL);
-
 
 main();
