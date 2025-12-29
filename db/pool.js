fbf82cee7fdc20b917afc5992d4271c2c981eb9d
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-12-29 11:30:12 +1100

    Add conditionals to environmentals for prod/dev deployment

--- db/pool.js
+++ db/pool.js
@@ -1,21 +1,30 @@
-require("dotenv").config()
+require("dotenv").config();
 const { Pool } = require("pg");
 const fs = require("fs");
 
+const env = process.env.NODE_ENV || "development";
 
-module.exports = new Pool({
+const config = env === "production" ? {
+    host: process.env.PROD_DB_HOST,
+    user: process.env.PROD_DB_USER,
+    database: process.env.PROD_DB_NAME,
+    password: process.env.PROD_DB_PASSWORD,
+    port: process.env.PROD_DB_PORT,
+    // ssl: {
+    //     rejectUnauthorized: true,
+    //     ca: fs.readFileSync("./ca.pem").toString(),
+    // },
+} : {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
-//      ssl: {
-//     rejectUnauthorized: true,
-//     ca: fs.readFileSync("./ca.pem").toString(),
-//   },
+};
+
+module.exports = new Pool({
+    ...config,
     max: 5,
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 2000,
-})
-
-
+});
